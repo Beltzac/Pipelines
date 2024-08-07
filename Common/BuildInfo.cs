5d75c3bfa08fd48ee5505b05dfbf7c316ce6562a
Alexandre Beltzac <ahoy.abeltzac@tcp.com.br>
2024-08-07 15:25:45 -0300

    Simplificar model

--- Common/BuildInfo.cs
+++ Common/BuildInfo.cs
@@ -1,13 +1,43 @@
-﻿using LiteDB;
-using System.Text.Json;
+﻿public class Commit
+{
+    public string Id { get; set; }
+    public string Message { get; set; }
+    public string Url { get; set; }
+    public string AuthorName { get; set; }
+    public string AuthorEmail { get; set; }
+}
 
-public class BuildInfo
+public class Build
 {
     public int Id { get; set; }
-    public BsonDocument Project { get; set; }
-    public BsonDocument Pipeline { get; set; }
-    public BsonDocument LatestBuildDetails { get; set; }
-    public BsonDocument LatestBuildCommit { get; set; }
+    public string Status { get; set; }
+    public string Result { get; set; }
+    public string Url { get; set; }
+
     public string ErrorLogs { get; set; }
-    public bool Clonned { get; set; }
+
+    public DateTime? Queued { get; set; }
+    public DateTime Changed { get; set; }
+
+    public Commit Commit { get; set; }
+}
+
+public class Pipeline
+{
+    public int Id { get; set; }
+    public Build Last { get; set; }
+    public Build LastSuccessful { get; set; }
+}
+
+public class Repository
+{
+    public Guid Id { get; set; }
+    public string Project { get; set; }
+    public string Name { get; set; }
+    public string Url { get; set; }
+    public bool MasterClonned { get; set; }
+
+    public Pipeline Pipeline { get; set; }
+
+    public string Path => $"{Project}/{Name}";
 }
