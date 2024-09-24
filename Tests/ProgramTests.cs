c03d7fe2ff82c6e08858540c0fbdffa98b2787ad
Alexandre Beltzac <ahoy.abeltzac@tcp.com.br>
2024-09-24 14:19:23 -0300

    ajuste test

--- Tests/ProgramTests.cs
+++ Tests/ProgramTests.cs
@@ -1,15 +1,50 @@
 using BuildInfoBlazorApp.Data;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.TeamFoundation.Build.WebApi;
+using Microsoft.TeamFoundation.Core.WebApi;
+using Microsoft.TeamFoundation.SourceControl.WebApi;
+using Microsoft.VisualStudio.Services.Common;
+using Microsoft.VisualStudio.Services.WebApi;
+using Moq;
 
 namespace Common.Tests
 {
     public class ProgramTests
     {
+
         [Fact]
         public void Services_AreRegisteredCorrectly()
         {
             // Arrange
             var serviceCollection = new ServiceCollection();
+
+            var config = new ConfigModel
+            {
+                OrganizationUrl = "https://dummy.url",
+                PAT = "dummyPAT",
+                LocalCloneFolder = "C:\\Temp"
+            };
+
+            // Mock IConfigurationService
+            var mockConfigService = new Mock<IConfigurationService>();
+            mockConfigService.Setup(s => s.GetConfig()).Returns(config);
+            serviceCollection.AddSingleton(mockConfigService.Object);
+
+            // Mock VssConnection to prevent actual Azure calls
+            var mockVssConnection = new Mock<IVssConnection>();
+            serviceCollection.AddSingleton(mockVssConnection.Object);
+
+            // Optionally, mock the HTTP clients if they are used immediately upon registration
+            //var mockBuildHttpClient = new Mock<BuildHttpClient>(mockVssConnection.Object);
+            //serviceCollection.AddSingleton(mockBuildHttpClient.Object);
+
+            //var mockGitHttpClient = new Mock<GitHttpClient>(mockVssConnection.Object);
+            //serviceCollection.AddSingleton(mockGitHttpClient.Object);
+
+            //var mockProjectHttpClient = new Mock<ProjectHttpClient>(mockVssConnection.Object);
+            //serviceCollection.AddSingleton(mockProjectHttpClient.Object);
+
+            // Register other necessary services
             serviceCollection.AddCustomServices();
 
             var serviceProvider = serviceCollection.BuildServiceProvider();
