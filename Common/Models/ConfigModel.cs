5e64d75a8ec9a1f0330ea95b4fcb1210fb228efb
Alexandre Beltzac <ahoy.abeltzac@tcp.com.br>
2024-10-14 11:13:20 -0300

    feat: Add ConfigModel for configuration management in services

---
+++ Common/Models/ConfigModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Common.Models
+{
+    public class ConfigModel
+    {
+        public string PAT { get; set; }
+        public string OrganizationUrl { get; set; }
+        public string LocalCloneFolder { get; set; }
+        public string ConsulUrl { get; set; }
+        public string ConsulFolder { get; set; }
+        public string ConsulToken { get; set; }
+        public string RepositoryOwner { get; set; } = "beltzac";
+        public string Username { get; set; } = "Beltzac";
+        public string RepositoryName { get; set; } = "pipelines";
+        public string UserAgent { get; set; } = "TcpDash";
+        public string AccessToken { get; set; }
+    }
+}
