481c4924c316a2ff784863a4fbb9586580613fc1
Alexandre Beltzac <ahoy.abeltzac@tcp.com.br>
2024-10-14 11:13:20 -0300

    feat: Add ConfigModel for configuration management in services

--- Common/Services/ConfigurationService.cs
+++ Common/Services/ConfigurationService.cs
@@ -1,3 +1,4 @@
+using Common.Models;
 using System.Text.Json;
 
 public class ConfigurationService : IConfigurationService
@@ -63,19 +64,4 @@ public class ConfigurationService : IConfigurationService
         _config = config;
         await Task.Run(() => SaveConfig());
     }
-}
-
-public class ConfigModel
-{
-    public string PAT { get; set; }
-    public string OrganizationUrl { get; set; }
-    public string LocalCloneFolder { get; set; }
-    public string ConsulUrl { get; set; }
-    public string ConsulFolder { get; set; }
-    public string ConsulToken { get; set; }
-    public string RepositoryOwner { get; set; } = "beltzac";
-    public string Username { get; set; } = "Beltzac";
-    public string RepositoryName { get; set; } = "pipelines";
-    public string UserAgent { get; set; } = "TcpDash";
-    public string AccessToken { get; set; }
 }
