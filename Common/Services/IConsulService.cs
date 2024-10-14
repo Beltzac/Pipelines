af4f02ec5d48b6b50a34a2e0928fbc3b75fe9c65
Alexandre Beltzac <ahoy.abeltzac@tcp.com.br>
2024-10-14 13:12:58 -0300

    Salvar em pastas diferentes o normal e o recursivo.
    Esconder menu do oracle

--- Common/Services/IConsulService.cs
+++ Common/Services/IConsulService.cs
@@ -5,7 +5,6 @@ namespace Common.Services
     public interface IConsulService
     {
         Task DownloadConsulAsync(ConsulEnvironment consulEnv);
-        Task<List<string>> GetConsulKeys(ConsulEnvironment consulEnv);
         Task<Dictionary<string, ConsulKeyValue>> GetConsulKeyValues(ConsulEnvironment consulEnv);
         Task OpenInVsCode(ConsulEnvironment env);
         Task UpdateConsulKeyValue(ConsulEnvironment consulEnv, string key, string value);
