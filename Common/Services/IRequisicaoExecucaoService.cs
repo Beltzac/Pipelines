524fc8b3e8f9a4e61ca242d66aeacdf1eb56d6eb
Alexandre Beltzac <ahoy.abeltzac@tcp.com.br>
2024-10-28 10:56:10 -0300

    feat: Add IRequisicaoExecucaoService interface

---
+++ Common/Services/IRequisicaoExecucaoService.cs
@@ -0,0 +1,9 @@
+﻿using Common.Models;
+
+namespace Common.Services
+{
+    public interface IRequisicaoExecucaoService
+    {
+        Task<List<RequisicaoExecucao>> ExecuteQueryAsync(string environment, DateTime? startDate = null, string? urlFilter = null, string? httpMethod = null, string[]? containerNumbers = null, string? nomeFluxo = null, int? userId = null, int? execucaoId = null, int maxRows = 10);
+    }
+}
