b0e024b41891f39224eae0a21851a76e03e86b85
Alexandre Beltzac <ahoy.abeltzac@tcp.com.br>
2024-11-06 15:39:04 -0300

    Mostrar projetos utilizados antes

---
+++ Common/Utils/WindowUtils.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanara.PInvoke;
+
+namespace Common.Utils
+{
+    public static class WindowUtils
+    {
+        public static List<string> EnumerarJanelas()
+        {
+            var windowList = new List<string>();
+
+            User32.EnumWindows((hWnd, lParam) =>
+            {
+                if (User32.IsWindowVisible(hWnd))
+                {
+                    int length = User32.GetWindowTextLength(hWnd);
+                    StringBuilder title = new StringBuilder(length + 1);
+                    User32.GetWindowText(hWnd, title, title.Capacity);
+
+                    if (!string.IsNullOrWhiteSpace(title.ToString()))
+                    {
+                        windowList.Add(title.ToString());
+                        Console.WriteLine("Window: " + title.ToString());
+                    }
+                }
+                return true;
+            }, nint.Zero);
+            return windowList;
+        }
+    }
+}
