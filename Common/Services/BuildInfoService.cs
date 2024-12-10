615567bcc4a669336a521a224c6ba5370d0d0aa0
Alexandre Beltzac <beltzac@gmail.com>
2024-12-10 10:57:51 -0300

    Refactor namespaces and update dependencies
    Replaced `LibGit2Sharp.Commands` with `Commands` in `ConsulBackupJob.cs` and `OracleViewsBackupJob.cs`. Updated `OpenWithVSCode` method in `BuildInfoService.cs` to include an additional parameter `true`. Replaced `System.Text.Encoding` with `Encoding` in `ConsulService.cs`. Simplified `System.Diagnostics.Process.Start` calls in `OpenFolderUtils.cs`. Updated `OpenWithVSCode` method in `OpenFolderUtils.cs` to accept `disableExtensions` parameter, adding `--disable-extensions` argument if `true`. Updated `packages.lock.json` to reflect new versions of dependencies: `H.GeneratedIcons.System.Drawing` to `2.2.0`, `H.NotifyIcon` to `2.2.0`, `Microsoft.Win32.SystemEvents` to `9.0.0`, `System.Drawing.Common` to `9.0.0`, and updated version constraints for `H.NotifyIcon` to `[2.2.0, )`.

--- Common/Services/BuildInfoService.cs
+++ Common/Services/BuildInfoService.cs
@@ -409,7 +409,7 @@ namespace Common.Services
 
         public async Task OpenCloneFolderInVsCode()
         {
-            OpenFolderUtils.OpenWithVSCode(_logger, _localCloneFolder);
+            OpenFolderUtils.OpenWithVSCode(_logger, _localCloneFolder, true);
         }
 
         private async Task UpsertAndPublish(Repository buildInfo, bool notify = true)
