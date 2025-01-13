fb4cde174fbf208f9a9cf59fc2d42f2f2fcea9e2
Alexandre Beltzac <beltzac@gmail.com>
2025-01-13 17:02:51 -0300

    refatora o componente ProgressBar para melhorar a exibição de carregamento e atualiza a lógica de progresso em várias páginas

--- Common/Services/IOracleSchemaService.cs
+++ Common/Services/IOracleSchemaService.cs
@@ -6,10 +6,10 @@ namespace Common.Services
 {
     public interface IOracleSchemaService
     {
-        IEnumerable<OracleDiffResult> Compare(string sourceEnvName, string targetEnvName);
+        Task<IEnumerable<OracleDiffResult>> Compare(string sourceEnvName, string targetEnvName);
         Task<bool> TestConnectionAsync(string connectionString, string schema);
         Task<OracleViewDefinition> GetViewDefinitionAsync(string connectionString, string schema, string viewName);
         Task<IEnumerable<OracleViewDefinition>> GetViewDefinitionsAsync(string connectionString, string schema);
-        OracleDiffResult GetViewDiff(string viewName, string oldContent, string newContent);
+        Task<OracleDiffResult> GetViewDiff(string viewName, string oldContent, string newContent);
     }
 }
