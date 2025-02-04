438ff8b7529521151c7b22b30deec8fa6974cdbf
Alexandre Beltzac <beltzac@gmail.com>
2025-02-04 14:18:33 -0300

    tentar ajustar update

--- Common/Services/AutoUpdateService.cs
+++ Common/Services/AutoUpdateService.cs
@@ -33,10 +33,34 @@ namespace Common.Services
         {
 #if DEBUG
             // Return a default version when in debug mode.
-            return Version.Parse("0.0.0.0");
+            return Version.Parse("0.0.0");
 #else
-    // Return the version from your Git tag (or similar) in release mode.
-    return Version.Parse(ThisAssembly.Git.BaseTag);
+            // Return the version from your Git tag (or similar) in release mode.
+            try
+            {
+                return Version.Parse(ThisAssembly.Git.Tag);
+            }
+            catch
+            {
+            }
+
+            try
+            {
+                return Version.Parse(ThisAssembly.Git.BaseTag);
+            }
+            catch
+            {
+            }
+
+            try
+            {
+                return Version.Parse($"{ThisAssembly.Git.SemVer.Major}.{ThisAssembly.Git.SemVer.Minor}.{ThisAssembly.Git.SemVer.Patch}");
+            }
+            catch
+            {
+            }
+
+            return Version.Parse("0.0.0");
 #endif
         }
 
