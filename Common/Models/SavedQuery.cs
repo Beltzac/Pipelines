04a0f33b94494a18de05df9b3fb7d25d66255d9b
Alexandre Beltzac <beltzac@gmail.com>
2025-04-28 13:27:58 -0300

    results

--- Common/Models/SavedQuery.cs
+++ Common/Models/SavedQuery.cs
@@ -1,10 +1,20 @@
 namespace Common.Models
 {
+    public enum AssertStatus
+    {
+        NotRun,
+        OK,
+        Problems,
+        Error
+    }
+
     public class SavedQuery
     {
         public string Name { get; set; }
         public string QueryString { get; set; }
         public string QueryType { get; set; } // "SQL" or "MongoDB"
         public string Description { get; set; }
+        public AssertStatus LastRunStatus { get; set; }
+        public List<Dictionary<string, object>> LastRunResults { get; set; }
     }
 }
