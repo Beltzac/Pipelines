f4261cb41631469d203bc768476f5102e32574c1
Alexandre Beltzac <beltzac@gmail.com>
2025-08-05 17:19:55 -0300

    feat: Introduz estado para gerenciamento de seleção de ambiente e tabela/view na página GenerateEfCoreMapping

---
+++ TugboatCaptainsPlayground/Services/GenerateEfCoreMappingState.cs
@@ -0,0 +1,8 @@
+namespace TugboatCaptainsPlayground.Services
+{
+    public class GenerateEfCoreMappingState
+    {
+        public string SelectedEnvironment { get; set; }
+        public string SelectedTableOrView { get; set; }
+    }
+}
